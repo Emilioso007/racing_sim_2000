baa388338e864b64a17b1049b7de799a59043208
Elias Prest Andersen <eliaspandersen@outlook.dk>
2024-05-07 17:55:08 +0200

    Changed debug colours and fixed a bug where the player also was affected by the debug

--- src/main/java/com/eaej/ScreenClasses/Screens/Screen.java
+++ src/main/java/com/eaej/ScreenClasses/Screens/Screen.java
@@ -38,7 +38,10 @@ public class Screen {
         points.add(vehicle.getPosition().copy().add(right));
 
         showLines(points, 0xFF0000FF, 5);
-        debug(vehicle);
+
+        if (vehicle.playerID == Vehicle.PLAYER_AI) {
+            debug(vehicle);
+        }
     }
 
     public void showLines(ArrayList<PVector> points, int color, int size) {
@@ -72,24 +75,24 @@ public class Screen {
         if (debug) {
             // Draw predicted future position
             p.stroke(0);
-            p.fill(0);
+            p.fill(0, 255, 0);
             p.line(vehicle.getPosition().x, vehicle.getPosition().y,
                     vehicle.getFuturePosition().x, vehicle.getFuturePosition().y);
             p.ellipse(vehicle.getFuturePosition().x, vehicle.getFuturePosition().y, 4, 4);
 
             // Draw normal position
             p.stroke(0);
-            p.fill(0);
+            p.fill(0, 255, 0);
             p.ellipse(vehicle.getNormal().x, vehicle.getNormal().y, 4, 4);
 
             // Draw actual target (red if steering towards it)
-            p.stroke(0);
+            p.stroke(0, 255, 0);
             p.line(vehicle.getFuturePosition().x, vehicle.getFuturePosition().y, vehicle.getTarget().x,
                     vehicle.getTarget().y);
             if (vehicle.getWorldRecord() > vehicle.getRadius()) {
                 p.fill(255, 0, 0);
             } else {
-                p.fill(0); // Fill with default color
+                p.fill(0, 255, 0); // Fill with default color
             }
             p.noStroke();
             p.ellipse(vehicle.getTarget().x, vehicle.getTarget().y, 8, 8);
